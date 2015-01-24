611cea338481ee5427ad68ed258ae3cafb6decfc
Anders Schmidt Hansen <me@andersschmidt.com>
2015-01-24 14:38:55 +0100

    Added in Elixir BrowserSync by Ponko2, set up the file paths for live reloading

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,26 @@
 var elixir = require('laravel-elixir');
 
+require('laravel-elixir-browser-sync');
+
 /*
  |--------------------------------------------------------------------------
  | Elixir Asset Management
  |--------------------------------------------------------------------------
  |
  | Elixir provides a clean, fluent API for defining some basic Gulp tasks
- | for your Laravel application. By default, we are compiling the Less
+ | for your Laravel application. By default, we are compiling the Sass
  | file for our application, as well as publishing vendor resources.
  |
  */
 
 elixir(function(mix) {
-    mix.less('app.less');
+    mix
+    .sass('app.scss')
+    .browserSync([
+      'app/**/*',
+      'public/**/*',
+      'resources/views/**/*'
+    ], {
+      proxy: 'sidequest.app'
+    });
 });
