5a161a5eeb79de2e9956a18e2bfa489665c39b51
Anders Schmidt Hansen <me@andersschmidt.com>
2015-01-25 16:04:46 +0100

    Added Olark support to services include

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -10,11 +10,11 @@
 		@yield('content')
 
 		@include('base.footer')
-
 		@include('base.scripts')
 
 		@include('services.google-analytics')
 		@include('services.heap-analytics')
+		@include('services.olark-customer-support')
 
 	</body>
 </html>
