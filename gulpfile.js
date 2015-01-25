467527a35b430a41f51825775eb47e3ed5a86261
Anders Schmidt Hansen <me@andersschmidt.com>
2015-01-25 14:36:43 +0100

    Added Sassdoc support and custom SassDoc Elixir ingredient to the mix

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,24 @@
-var elixir = require('laravel-elixir');
+var elixir  = require('laravel-elixir'),
+    gulp    = require('gulp'),
+    sassdoc = require('sassdoc');
 
 require('laravel-elixir-browser-sync');
+
+/*
+ |--------------------------------------------------------------------------
+ | Elixir Custom Ingredients
+ |--------------------------------------------------------------------------
+ */
+
+  // Run SassDoc - Document your Sass in a nice way
+  elixir.extend('sassdoc', function(){
+    gulp.task('sassdoc', function(){
+      var options = { dest: 'public/docs/sass' };
+      return gulp.src('resources/assets/sass/**/*.scss').pipe(sassdoc(options));
+    });
+    this.registerWatcher('sassdoc', '**/*.scss');
+    return this.queueTask('sassdoc');
+  });
 
 /*
  |--------------------------------------------------------------------------
@@ -13,15 +31,16 @@ require('laravel-elixir-browser-sync');
  |
  */
 
-elixir(function(mix) {
-    mix
-    .sass('app.scss')
-    .coffee()
-    .browserSync([
-      'app/**/*',
-      'public/**/*',
-      'resources/views/**/*'
-    ], {
-      proxy: 'sidequest.app'
-    });
-});
+  elixir(function(mix) {
+      mix
+      .sass('app.scss')
+      .sassdoc()
+      .coffee()
+      .browserSync([
+        'app/**/*',
+        'public/**/*',
+        'resources/views/**/*'
+      ], {
+        proxy: 'sidequest.app'
+      });
+  });
