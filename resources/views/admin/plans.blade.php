45c9a3bc62bd53835ec7cac6b1230d10a8b2141e
Anders Schmidt Hansen <me@andersschmidt.com>
2015-03-01 20:32:32 +0100

    Added check for secret stripe api key at plans

--- resources/views/admin/plans.blade.php
+++ resources/views/admin/plans.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
   @if( count($plans) )
-    <p>Changed something else than description and features? <a href="./import-subscription-plans">Re-import now</a>.</p>
+    <p>Changed something else than description and features? <a href="{{ url('admin/import-subscription-plans') }}">Re-import now</a>.</p>
     <div class="row">
     @foreach( $plans as $plan )
       <div class="col-sm-4">
@@ -12,7 +12,13 @@
     @endforeach
     </div>
   @else
-    <p>You haven't imported your Stripe plans yet... <a href="./import-subscription-plans">Import now</a>.</p>
+
+    @if( env('SERVICE_STRIPE_SECRET_API_KEY') == null )
+    <p>You haven't set your <strong>secret Stripe API key</strong> yet... Open you <em>.env</em> file and set it first.</p>
+    @else
+    <p>You haven't imported your Stripe plans yet... <a href="{{ url('admin/import-subscription-plans') }}">Import now</a>.</p>    
+    @endif
+    
   @endif
 
 @endsection
