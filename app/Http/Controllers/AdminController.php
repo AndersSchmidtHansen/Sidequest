060e53f09bd3e44acf72555352f76cf380240ce6
Anders Schmidt Hansen <me@andersschmidt.com>
2015-03-12 22:48:28 +0100

    fixed saving issue at admin

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -71,7 +71,7 @@ class AdminController extends Controller {
   public function postUpdateSettings()
   {
 
-    $setting = $this->settings;
+    $setting = ApplicationSetting::find(1);
 
     $exceptions = [
       '_token',
