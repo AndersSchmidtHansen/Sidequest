3c58feffd93a03d235ab02728d9fed778be44918
Anders Schmidt Hansen <me@andersschmidt.com>
2015-05-24 22:34:20 +0200

    added small existence check for plans at pricing page

--- resources/views/modules/plans/all.blade.php
+++ resources/views/modules/plans/all.blade.php
@@ -1,3 +1,7 @@
-@foreach($plans as $plan)
-  @include('modules.plans.plan')
-@endforeach
+@if( count($plans) > 0 )
+  @foreach($plans as $plan)
+    @include('modules.plans.plan')
+  @endforeach
+@else
+  <p>You haven't imported any plans yet! You can do that from the admin panel.</p>
+@endif
